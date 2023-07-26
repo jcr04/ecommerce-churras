6f610cd6735e3b854c619c0bb6ecd5ae276b68c7
Edison de F. Ventura Junior <122493018+Ventura-Jr@users.noreply.github.com>
2023-07-26 13:39:04 -0300

    teste-1

--- Server/data/Products.js
+++ Server/data/Products.js
@@ -26,24 +26,7 @@ const products = [
     name: "Linguiça de frango com bacon ",
     image: "/images/4.png",
     description:
-    <p>Sem conservantes, congelamento rápido, sem hormônios.<br>
-    </br><br>
-    </br>INFORMAÇÕES NUTRICIONAIS:<br>
-    </br>Porção de 50g (1/2 und)<br>
-    </br><br>
-    </br>Valor energético - 95kcal / 399kJ - 5%<br>
-    </br>Carboidratos - 1g - 0%<br>
-    </br>Proteínas - 7g - 9%<br>
-    </br>Gorduras totais - 7g - 13%<br>
-    </br>Gorduras saturadas - 2,1g - 10%<br>
-    </br>Gorduta trans - 0g - (**%)<br>
-    </br>Fibra alimentar - 0g - 0%<br>
-    </br>Sódio - 440mg - 16%<br>
-    </br><br>
-    </br>*% Valores diários com base em uma dieta de 2.000kcal ou 8.400kJ. Seu valores
-    podem ser maiores ou menores dependendo de suas necessidades energéticas.<br>
-    </br>**VD não estabelecido.
-    </p>,
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
     price: 18,
     countInStock: 100,
     rating: 3.5,
@@ -54,24 +37,7 @@ const products = [
     name: "Coxa de frango",
     image: "/images/3.png",
     description:
-      <p>Sem conservantes, congelamento rápido, sem hormônios.<br>
-      </br><br>
-      </br>INFORMAÇÕES NUTRICIONAIS:<br>
-      </br>Porção de 82g (1/2 und)<br>
-      </br><br>
-      </br>Valor energético - 125kcal / 525kJ - 6%<br>
-      </br>Carboidratos - 0g - 0%<br>
-      </br>Proteínas - 15g - 20%<br>
-      </br>Gorduras totais - 7,4g - 13%<br>
-      </br>Gorduras saturadas - 1,6g - 7%<br>
-      </br>Gorduta trans - 0g - (**%)<br>
-      </br>Fibra alimentar - 0g - 0%<br>
-      </br>Sódio - 93mg - 4%<br>
-      </br><br>
-      </br>*% Valores diários com base em uma dieta de 2.000kcal ou 8.400kJ. Seu valores
-      podem ser maiores ou menores dependendo de suas necessidades energéticas.<br>
-      </br>**VD não estabelecido.
-      </p>,
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
     price: 15,
     countInStock: 100,
     rating: 5,
@@ -82,24 +48,7 @@ const products = [
     name: "Asa inteira de frango",
     image: "/images/2.png",
     description:
-    <p>Sem conservantes, congelamento rápido, sem hormônios.<br>
-    </br><br>
-    </br>INFORMAÇÕES NUTRICIONAIS:<br>
-    </br>Porção de 70g (1/4 und)<br>
-    </br><br>
-    </br>Valor energético - 126kcal / 529kJ - 6%<br>
-    </br>Carboidratos - 0g - 0%<br>
-    </br>Proteínas - 13g - 17%<br>
-    </br>Gorduras totais - 8,4g - 15%<br>
-    </br>Gorduras saturadas - 2,1g - 10%<br>
-    </br>Gorduta trans - 0g - (**%)<br>
-    </br>Fibra alimentar - 0g - 0%<br>
-    </br>Sódio - 60mg - 3%<br>
-    </br><br>
-    </br>*% Valores diários com base em uma dieta de 2.000kcal ou 8.400kJ. Seu valores
-    podem ser maiores ou menores dependendo de suas necessidades energéticas.<br>
-    </br>**VD não estabelecido.
-    </p>,
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
     price: 25,
     countInStock: 100,
     rating: 4,
@@ -110,24 +59,7 @@ const products = [
     name: "Coração de frango",
     image: "/images/1.png",
     description:
-      <p>Sem conservantes, congelamento rápido, sem hormônios.<br>
-      </br><br>
-      </br>INFORMAÇÕES NUTRICIONAIS:<br>
-      </br>Porção de 100g (10 unds)<br>
-      </br><br>
-      </br>Valor energético - 125kcal / 525kJ - 6%<br>
-      </br>Carboidratos - 0g - 0%<br>
-      </br>Proteínas - 11g - 15%<br>
-      </br>Gorduras totais - 9g - 16%<br>
-      </br>Gorduras saturadas - 1,7g - 8%<br>
-      </br>Gorduta trans - 0g - (**%)<br>
-      </br>Fibra alimentar - 0g - 0%<br>
-      </br>Sódio - 75mg - 3%<br>
-      </br><br>
-      </br>*% Valores diários com base em uma dieta de 2.000kcal ou 8.400kJ. Seu valores
-      podem ser maiores ou menores dependendo de suas necessidades energéticas.<br>
-      </br>**VD não estabelecido.
-      </p>,
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
     price: 22,
     countInStock: 100,
     rating: 4,
