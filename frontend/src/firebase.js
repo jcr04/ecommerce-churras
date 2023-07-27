9ec04e789b3513c8695824a5947e456c14e912d2
jcr04 <joaocarlosreboucas15@gmail.com>
2023-07-27 19:51:40 -0300

    addign firebase authentication

---
+++ frontend/src/firebase.js
@@ -0,0 +1,19 @@
+import "firebase/auth";
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtJVNGzRic7QL5NwSux1viw5kxAP_X4iY",
+    authDomain: "bbq-login-auth.firebaseapp.com",
+    projectId: "bbq-login-auth",
+    storageBucket: "bbq-login-auth.appspot.com",
+    messagingSenderId: "188635112340",
+    appId: "1:188635112340:web:1e4704f97dfa1753906581"
+
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  
+  export const auth = firebase.auth();
+  export default firebase;
